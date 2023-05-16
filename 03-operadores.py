e8cc2a7361d739e721a834919459f1695c29b92c
GuillermoTapia91 <guillermo.tapia1991@gmail.com>
2023-05-16 15:26:05 -0500

    semana01

---
+++ 03-operadores.py
@@ -0,0 +1,16 @@
+# Operadores Lógicos
+
+edad_juan = 10
+edad_jonathan = 15
+edad_roxana = 18
+edad_martina = 19
+
+# and (y) &&
+
+print((edad_martina > edad_juan) and (edad_martina > edad_roxana))
+
+print((edad_jonathan > edad_juan) and (edad_roxana > edad_martina))
+
+# or (o)
+
+print(( edad_jonathan > edad_juan) or (edad_roxana > edad_martina))
